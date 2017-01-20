cf0e02f77229d6da22fc4f3a3d7e9d4f5963c152
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-01-20 13:30:47 +0200

    Project upload and ReactNative upgrade

--- index.android.js
+++ index.android.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+/*
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
+import {authReducer} from './src/auth';
+import {Router} from './src/Router';
 
-class HomeworkOrganiser extends Component {
-  render() {
-    return (
-      <Text>Hello world!</Text>
-    );
-  }
+
+const rootReducer = combineReducers({ auth: authReducer});
+const store = createStore(rootReducer, applyMiddleware(thunk, createLogger({colors: {}})));
+
+export default class ReactClient extends Component {
+    render() {
+        return (
+            <Router store={store}/>
+        );
+    }
 }
 
-AppRegistry.registerComponent('HomeworkOrganiser', () => HomeworkOrganiser);
+AppRegistry.registerComponent('ReactClient', () => ReactClient);
+*/
